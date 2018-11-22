6c8b54eb28abd92d1cae6c97ee5f96cfe3a56a77
Max Beier <beierm@htw-berlin.de>
2018-11-22 17:51:21 +0100

    Updates

---
+++ precache-manifest.d847f6f33672b158bce88a726e260eb3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9f70b2235e03eeec59f",
+    "url": "/tawian/static/js/main.d9f70b22.chunk.js"
+  },
+  {
+    "revision": "88ece8a67bc5bbcb28e7",
+    "url": "/tawian/static/css/1.7be50f23.chunk.css"
+  },
+  {
+    "revision": "88ece8a67bc5bbcb28e7",
+    "url": "/tawian/static/js/1.88ece8a6.chunk.js"
+  },
+  {
+    "revision": "d17257e324c3e1756535",
+    "url": "/tawian/static/js/runtime~main.d17257e3.js"
+  },
+  {
+    "revision": "b548795ec7dcb0d61861740c7e301a97",
+    "url": "/tawian/static/media/cousine-latin-400.b548795e.woff"
+  },
+  {
+    "revision": "9ea5649e7f6788bc5eb814ea946d8bae",
+    "url": "/tawian/static/media/cousine-latin-400.9ea5649e.woff2"
+  },
+  {
+    "revision": "43f9b4a11c1c0d43cfbcaa593c97c6b5",
+    "url": "/tawian/static/media/cousine-latin-400italic.43f9b4a1.woff"
+  },
+  {
+    "revision": "3bb8471cdd3c01ffa606d3a0e1eb32eb",
+    "url": "/tawian/static/media/cousine-latin-400italic.3bb8471c.woff2"
+  },
+  {
+    "revision": "11d4aa02dfbaeef7599c0b82a00e09aa",
+    "url": "/tawian/static/media/cousine-latin-700.11d4aa02.woff2"
+  },
+  {
+    "revision": "e5f0410280806c39d94a0a37c11507c6",
+    "url": "/tawian/static/media/cousine-latin-700.e5f04102.woff"
+  },
+  {
+    "revision": "9ff9c02c2350c58c3eebf849354a3849",
+    "url": "/tawian/static/media/cousine-latin-700italic.9ff9c02c.woff2"
+  },
+  {
+    "revision": "d0dcb79a78572fa7e91a5685adc16c01",
+    "url": "/tawian/static/media/cousine-latin-700italic.d0dcb79a.woff"
+  },
+  {
+    "revision": "1a89788cf1810e82aa77429497a04023",
+    "url": "/tawian/index.html"
+  }
+];
